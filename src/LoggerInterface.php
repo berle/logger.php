965c1f972d59d21d466849b402546acd81de1fd0
anders@berle.cc <anders@berle.cc>
2018-08-23 09:56:45 +0000

    Fix interface

--- src/LoggerInterface.php
+++ src/LoggerInterface.php
@@ -5,7 +5,6 @@ namespace Berle\Logger;
 interface LoggerInterface
 {
 
-    public function logger(): LoggerInterface;
     public function debug(string $message, array $data = []): void;
     public function info(string $message, array $data = []): void;
     public function warn(string $message, array $data = []): void;
